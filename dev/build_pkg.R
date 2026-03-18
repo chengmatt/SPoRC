eb9214eb486fbd6e73e2e08ccb778871010790e0
Matt Cheng <84427627+chengmatt@users.noreply.github.com>
2026-03-17 20:58:14 -0800

    Update build_pkg.R

--- dev/build_pkg.R
+++ dev/build_pkg.R
@@ -5,60 +5,45 @@ library(attachment)
 library(usethis)
 library(here)
 
-# Build Package -----------------------------------------------------------
+# Authentication -----------------------------------------------
 # usethis::create_github_token()
 # gitcreds::gitcreds_set()
+Sys.unsetenv("GITHUB_PAT") # unset before building vignettes
 
+# Documentation -----------------------------------------------------------
 att_amend_desc(update = TRUE)
-document() # document functions
-roxygenise() # make sure functions have roxygen documentation
-desc::desc_del_dep("compResidual", "Imports") # move to suggests
-desc::desc_set_dep("compResidual", "Suggests") # move to suggests
-desc::desc_del_dep("remotes", "Imports") # remove remotes
-Sys.unsetenv("GITHUB_PAT") # may need to unset to build vignettes
+document()
+roxygenise()
 
-# website stuff
-Sys.unsetenv("GITHUB_PAT") # may need to unset to build vignettes
+# Dependency adjustments
+desc::desc_del_dep("compResidual", "Imports")
+desc::desc_set_dep("compResidual", "Suggests")
+desc::desc_del_dep("remotes", "Imports")
+
+# pkgdown Site ------------------------------------------------------------
 pkgdown::clean_cache()
-# usethis::use_pkgdown_github_pages()
 pkgdown::check_pkgdown()
 pkgdown::build_site()
 pkgdown::build_news()
 servr::httd("docs")
+
+# One-time setup
+# usethis::use_pkgdown_github_pages()
 # pkgdown::build_site(preview = TRUE)
 
+# Build Ignore ------------------------------------------------------------
+use_build_ignore("dev")
+usethis::use_build_ignore("_pkgdown.yml")
 
-use_build_ignore("dev") # ignore dev folder
-usethis::use_build_ignore("_pkgdown.yml") # ignore pkgdown.yml
-check() # check package stuff
-Sys.unsetenv("GITHUB_PAT") # may need to unset to build vignettes
-build() # build package
-install() # install locally
-unloadNamespace('SPoRC')
+# Check, Build & Install --------------------------------------------------
+check()
+build()
+install()
+unloadNamespace("SPoRC")
 
-# Do tests
-# devtools::test()
+# Vignettes ---------------------------------------------------------------
+# build_vignettes()
 
-# Gitignore
-# usethis::edit_git_ignore()
-# unignore dev folder
-# rbuildignore <- readLines(".Rbuildignore")
-# rbuildignore <- rbuildignore[!grepl("^\\^dev\\$", rbuildignore)]
-# writeLines(rbuildignore, ".Rbuildignore")
-
-# Add news
-# usethis::use_news_md() # add news md
-
-# Unit Tests --------------------------------------------------------------
-# usethis::use_testthat()
-# usethis::use_test("dusky_rtmb")
-# usethis::use_test("sabie_sgl_rtmb")
-# usethis::use_test("ebs_pol_sgl_rtmb")
-# usethis::use_test("sabie_three_rg_rtmb")
-# usethis::use_test("sgl_rg_simple_sim_test")
-
-# Build Vignettes ---------------------------------------------------------
-# build_vignettes() # build vignettes
 # usethis::use_vignette("a_model_dimensions")
 # usethis::use_vignette("b_model_parameters")
 # usethis::use_vignette("c_model_equations")
@@ -76,3 +61,24 @@ unloadNamespace('SPoRC')
 # usethis::use_vignette("o_get_started")
 # usethis::use_vignette("p_single_region_dusky_alt_mp_testing")
 # usethis::use_vignette("q_movement_param")
+
+
+# Unit Tests --------------------------------------------------------------
+# devtools::test()
+
+# usethis::use_testthat()
+# usethis::use_test("dusky_rtmb")
+# usethis::use_test("sabie_sgl_rtmb")
+# usethis::use_test("ebs_pol_sgl_rtmb")
+# usethis::use_test("sabie_three_rg_rtmb")
+# usethis::use_test("sgl_rg_simple_sim_test")
+
+
+# Misc One-Time Setup -----------------------------------------------------
+# usethis::use_news_md()
+# usethis::edit_git_ignore()
+
+# Undo dev/ build ignore:
+# rbuildignore <- readLines(".Rbuildignore")
+# rbuildignore <- rbuildignore[!grepl("^\\^dev\\$", rbuildignore)]
+# writeLines(rbuildignore, ".Rbuildignore")

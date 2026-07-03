29b5c2b75e63e9b0bbd2be2e87fd56b03ed1f6a5
Matt Cheng <84427627+chengmatt@users.noreply.github.com>
2026-07-02 22:01:36 -0800

    Update test-internal_popsp_comp_tag_osas.R

--- tests/testthat/test-internal_popsp_comp_tag_osas.R
+++ tests/testthat/test-internal_popsp_comp_tag_osas.R
@@ -508,7 +508,7 @@ test_that("OSA residuals are well-calibrated under correct EM and mis-calibrated
     }
 
     list(
-      mean = mean(resid), sd = sd_val, shapiro_p = shapiro_p, trend_p = trend_p,
+      mean = mean(resid), sd = sd_val, shapiro_p = shapiro_p,
       mean_ok = mean_ok, sd_ok = sd_ok, normal_ok = normal_ok,
       autocorr_ok = autocorr_ok,
       calibrated = mean_ok && sd_ok && normal_ok && autocorr_ok && trend_ok
